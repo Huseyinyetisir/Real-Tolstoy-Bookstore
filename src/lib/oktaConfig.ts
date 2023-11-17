83e0ef1140ebfdaafefc768cbab21b7ac6002c57
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-11-17 14:13:01 +0300

    Okta implemented for authentication

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oadb764xnuBk2dIF5d7',
+    issuer: 'https://dev-71513886.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid','profile','email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
