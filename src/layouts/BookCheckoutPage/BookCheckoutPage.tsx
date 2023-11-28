413de25e1f7d22cc926fda9d51c30e296d895cd5
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-11-28 23:19:15 +0300

    Checkout page edited for reviews

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -26,6 +26,11 @@ export const BookCheckoutPage = () => {
 
     const bookId = (window.location.pathname).split('/')[2];
 
+    // Is Book Check Out?
+    const [isCheckedOut, setIsCheckedOut] = useState(false);
+    const [isLoadingBookCehckedOut, setIsLoadingBookCehckedOut] = useState(true);
+
+    // Fetch Book Effect
     useEffect(() => {
 
         const fetchBook = async () => {
@@ -57,11 +62,12 @@ export const BookCheckoutPage = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, []);
+    }, [isCheckedOut]);
 
+    // Fetch Book Reviews Effect
     useEffect(() => {
         const fetchBookReviews = async () => {
-            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
 
             const responseReviews = await fetch(reviewUrl);
 
@@ -84,16 +90,17 @@ export const BookCheckoutPage = () => {
                     date: responseData[key].date,
                     rating: responseData[key].rating,
                     bookId: responseData[key].bookId,
-                    reviewDescription: responseData[key].reviewDescription
+                    reviewDescription: responseData[key].reviewDescription,
                 });
                 weightedStarReviews = weightedStarReviews + responseData[key].rating;
             }
 
             if (loadedReviews) {
-                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 1).toFixed(1));
+                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
                 setTotalStars(Number(round));
+                setReviews(loadedReviews);
             }
-            setReviews(loadedReviews);
+
             setIsLoadinReview(false);
         };
 
@@ -101,8 +108,8 @@ export const BookCheckoutPage = () => {
             setIsLoadinReview(false);
             setHttpError(error.message);
         })
-    })
-
+    } )
+    // Fetch Current Loans Count Effect
     useEffect(() => {
         const fetchUserCuurentLoansCount = async () => {
             if (authState && authState.isAuthenticated) {
@@ -127,9 +134,35 @@ export const BookCheckoutPage = () => {
             setIsLoadingCurrentLoansCount(false);
             setHttpError(error.message);
         })
+    }, [authState, isCheckedOut]);
+    // Fetch User Checked Out Effect
+    useEffect(() => {
+        const fetchUserCheckedOut = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/books/secure/ischeckedout/byuser/?bookId=${bookId}`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const checkedOutResponse = await fetch(url, requestOptions);
+                if (!checkedOutResponse.ok) {
+                    throw new Error('Something went wrong!');
+                }
+                const checkedOutResponseJson = await checkedOutResponse.json();
+                setIsCheckedOut(checkedOutResponseJson);
+            }
+            setIsLoadingBookCehckedOut(false);
+        }
+        fetchUserCheckedOut().catch((error: any) => {
+            setIsLoadingBookCehckedOut(false);
+            setHttpError(error.message);
+        })
     }, [authState]);
 
-    if (isLoading || isLoadinReview || isLoadingCurrentLoansCount) {
+    if (isLoading || isLoadinReview || isLoadingCurrentLoansCount || isLoadingBookCehckedOut) {
         return (
             <SpinnerLoading />
         );
@@ -142,7 +175,21 @@ export const BookCheckoutPage = () => {
         );
     }
 
-
+    async function checkoutBook(){
+        const url = `http://localhost:8080/api/books/secure/checkout/?bookId=${book?.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content/type': 'application/json'
+            }
+        };
+        const checkoutResponse = await fetch(url,requestOptions);
+        if (!checkoutResponse.ok){
+            throw new Error('Something went wrong!');
+        } 
+        setIsCheckedOut(true);
+    } 
 
     return (
         <div>
@@ -164,7 +211,9 @@ export const BookCheckoutPage = () => {
                             <StarsReview Rating={totalStars} Size={32} />
                         </div>
                     </div>
-                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}/>
+                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount} 
+                        isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
+                        checkoutBook={checkoutBook}/>
                 </div>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -186,7 +235,9 @@ export const BookCheckoutPage = () => {
                         <StarsReview Rating={totalStars} Size={32} />
                     </div>
                 </div>
-                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}/>
+                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}
+                    isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
+                    checkoutBook={checkoutBook}/>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
